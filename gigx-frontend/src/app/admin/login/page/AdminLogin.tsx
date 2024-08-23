380040b38208801e520d6f78cd7442b798ea9204
hudhyfa <hudhyfahbk@gmail.com>
2024-08-24 01:56:20 +0530

    added admin login UI

--- gigx-frontend/src/app/admin/login/page/AdminLogin.tsx
+++ gigx-frontend/src/app/admin/login/page/AdminLogin.tsx
@@ -1,97 +1,114 @@
 "use client";
 
 import { useState } from "react";
+import axios from "@/utils/axiosConfig";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation"
 
 const schema = z.object({
-    email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(8, { message: "Password must be at least 8 characters."})
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters." })
+    .max(15, {message: "Password must be at most 15 characters."})
 });
 
 type FormData = z.infer<typeof schema>;
 
 export default function AdminLogin() {
-    const [error, setError] = useState('');
-    const router = useRouter();
+  const [error, setError] = useState<string>("");
+  const router = useRouter();
 
-    const {register, handleSubmit, formState: { errors}} = useForm<FormData>({
-        resolver: zodResolver(schema)
-    });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
-    const onSubmit = async (data: FormData) => {
-        const result = await signIn('credentials', {
-            redirect: false,
-            email: data.email,
-            password: data.password
-        })
-        if(result?.error) {
-            setError("Invalid credentials");
-        } else {
-            router.push('/admin')
-        }
+  const onSubmit = async (data: FormData) => {
+    try {
+      console.log("inside on submit")
+      // Send login request to API gateway
+      const response = await axios.post("/user-service/admin/login", {
+        email: data.email,
+        password: data.password,
+      });
+      console.log(response);
+
+      // Save JWT in cookies
+      document.cookie = `token=${response.data.token}; path=/;`;
+
+      // Redirect to admin dashboard
+      router.push("/admin");
+    } catch (err) {
+      setError("Invalid credentials");
     }
+  };
 
-    return(
-        <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Admin Login
-            </h2>
-          </div>
-          <form className="mt-8 space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div>
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email-address"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
-                  {...register('email')}
-                />
-                {errors.email && <p className="mt-2 text-sm text-red-600">{errors.email.message}</p>}
-              </div>
-              <div>
-                <label htmlFor="password" className="sr-only">
-                  Password
-                </label>
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
-                  {...register('password')}
-                />
-                {errors.password && <p className="mt-2 text-sm text-red-600">{errors.password.message}</p>}
-              </div>
-            </div>
-  
-            {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
-  
-            <div>
-              <button
-                type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Sign in
-              </button>
-            </div>
-          </form>
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-blue sm:px-6 lg:px-8">
+      <div className="max-w-md w-full h-auto border bg-white p-8 rounded-xl">
+        <div>
+          <h2 className="text-center text-3xl font-extrabold text-blue font-atkinson">
+            GIGGX
+          </h2>
         </div>
-      </div>  
-    )
-}
+        <form className="mt-8" onSubmit={handleSubmit(onSubmit)}>
+          <div className="rounded-md">
+            <div className="mb-5">
+              <label htmlFor="email-address" className="font-atkinson font-bold text-lg tracking-wide">
+                Email
+              </label>
+              <input
+                id="email-address"
+                // name="email"
+                type="email"
+                autoComplete="email"
+                className="w-full px-3 py-3 border-2 border-gray-30 text-blackfaded rounded-md font-atkinson focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                {...register("email")}
+              />
+              {errors.email && (
+                <p className="mt-1 font-atkinson text-sm text-red-600 tracking-tight font-thin">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div className="mb-9">
+              <label htmlFor="password" className="font-atkinson font-bold text-lg tracking-wide">
+                Password
+              </label>
+              <input
+                id="password"
+                // name="password"
+                type="password"
+                autoComplete="current-password"
+                className="w-full px-3 py-3 border-2 border-gray-30 text-blackfaded rounded-md font-atkinson focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                {...register("password")}
+              />
+              {errors.password && (
+                <p className="mt-1 font-atkinson text-sm text-red-600 tracking-tight font-thin">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+          </div>
 
+          {error && <p className="mt-2 text-sm text-red-600">{error}</p>}
+
+          <div className="mt-3">
+            <button
+              type="submit"
+              className="w-full flex justify-center py-3 px-4 border border-transparent text-md font-atkinson font-bold rounded-md text-white bg-blue hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
